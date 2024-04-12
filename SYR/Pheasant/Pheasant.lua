c3938b5db19a72c5615377fac44e8c9e8fbbd249
Shufflepuff <mail@shuffla.dk>
2024-04-12 16:08:33 +0200

    Further expanding on the Pheasant story script

--- SYR/Pheasant/Pheasant.lua
+++ SYR/Pheasant/Pheasant.lua
@@ -22,13 +22,22 @@ local Pheasant = {
         },
     },
     MSG = {
-        Start = _weco ..", [CALLSIGN]. We've received intel that a motor convoy carrying supplies and troops is driving along " .. _msr1 .. " towards " ..
-        _destination .. ". Request retasking of Harriers to intercept and destroy A S A P. E T A of convoy at " ..
-        _destination .. " is time plus one hour and fifteen.",
-        SCAR_Request = _weco .. "[CALLSIGN] ",
-        MissionFailed = _weco .. "[CALLSIGN] ",
-        MissionComplete = _weco .. "[CALLSIGN] ",
-        Pheasant_Urgent = _weco .. "[CALLSIGN] ",
+        Start =
+            _weco .. ", [CALLSIGN]. New mission, codename: " .. _codeword .. ". We've received intel that a motor convoy carrying supplies and troops is driving along "
+            .. _msr1 .. " towards " .. _destination .. ". Request retasking of Harriers to intercept and destroy A S A P. E T A of convoy at " ..
+            _destination .. " is time plus one hour and fifteen. [CALLSIGN] out.",
+        SCAR_Request =
+            _weco .. "[CALLSIGN], [CALLSIGN] out.",
+        MissionFailed =
+            _weco ..
+            "[CALLSIGN], mission " .. _codeword .. " has failed. The motor convoy has reached " .. _destination .. " and have succesfully "
+            .. "resupplied and reinforced the forces there. [CALLSIGN] out.",
+        MissionComplete =
+            _weco .. "[CALLSIGN], mission " .. _codeword .. ": motor convoy succesfully destroyed, and enemy forces at "
+            .. _destination .. " are severely weakened in their ability to maintain control of the base. Good work. [CALLSIGN] out.",
+        Pheasant_Urgent =
+            _weco .. "[CALLSIGN], update on " .. _codeword .. ". The motor convoy has just crossed the bridge north east of Manjib into the N F Z. Unless action is taken immediately, "
+            .. "they are slated to arrive at " .. _destination .. " in .[CALLSIGN] out.",
     }
 }
 
@@ -55,8 +64,17 @@ function Pheasant:MissionFailed()
     self:Send(self.MSG.MissionFailed)
 end
 
+function Pheasant:Urgent()
+    self:Send(self.MSG.Pheasant_Urgent)
+end
+
 function Pheasant:MissionComplete()
-    self:Send(self.MSG.MissionComplete)
+    DCAF.startScheduler(function()
+        if not self.Groups.RED.Pheasant:IsActive() then return end
+        if not self.Groups.RED.Pheasant:IsAlive() then
+            self:Send(self.MSG.MissionComplete)
+        end
+    end, Minutes(1))
 end
 
 Pheasant._main_menu = GM_Menu:AddMenu(_codeword)
