035c83bf1f123e4eee63c309f81be1f2a982660e
Shufflepuff <mail@shuffla.dk>
2024-04-15 16:29:31 +0200

    WIP Commit

--- SYR/Albatroz/Albatroz.lua
+++ SYR/Albatroz/Albatroz.lua
@@ -1,13 +1,16 @@
 -- //////////////////////////////////////////////////////////////////////////////////
 --                                    ALBATROZ
 --                                    ********
--- Syrian motor convoy departs to _site for emergency repairs of _targetVehicleType
--- revealing the location of _maintenanceBuilding for a future Strike package if
--- if located within _revealDeadline.
+-- Syrian motor convoy departs to _destination for emergency repairs of _targetVehicleType
+-- revealing the location of the vehicledepot for a future Strike package if located within _revealDeadline.
+
+-- ///////////////////////////////CONFIG SECTION\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+--                      CHANGE THESE TO REFLECT YOUR MISSION
 
 local _codeword = "Albatroz"
 local _ido = "FOCUS"
-local _gridStart = "p[LL XX]"
+local _vehicleDepotLocation =
+local _gridStart = "p[DV 17]"
 local _msr1 = "[Descriptor]"
 local _destination = "[Desto Name]"
 Albatroz = {
@@ -20,12 +23,14 @@ Albatroz = {
         },
     },
     MSG = {
-        Start = _ido .. "[CALLSIGN], ",
+        Start = _ido .. "[CALLSIGN]. We've received intel that a Syrian ",
         MissionFailed = _ido .. "[CALLSIGN], ",
         ConvoyDestroyed = _ido .. "[CALLSIGN], ",
         Albatroz_Urgent = _ido .. "[CALLSIGN], ",
     }
 }
+
+-- \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ ←     ↓     → ///////////////////////////////////
 
 function Albatroz:Start(tts)
     if self._is_started then return end
@@ -38,9 +43,11 @@ function Albatroz:Start(tts)
 end
 
 
+
 Albatroz._main_menu = GM_Menu:AddMenu(_codeword)
 Albatroz._start_menu = Pheasant._main_menu:AddCommand("Start", function()
     local tts
     if DCAF.TTSChannel then tts = DCAF.TTSChannel:New() end
     Albatroz:Start(tts)
 end)
+Trace(\\\\\\\\\)
