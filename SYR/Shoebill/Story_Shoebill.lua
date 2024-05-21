824a43208efb10febf8d8fd564ebab5b90fbe345
Shufflepuff <mail@shuffla.dk>
2024-05-21 20:14:04 +0200

    Update
    Chris :: Shoebill now only needs input from MD about JTAC frequency. Everything else has been tested and is working

--- SYR/Shoebill/Story_Shoebill.lua
+++ SYR/Shoebill/Story_Shoebill.lua
@@ -12,8 +12,8 @@
 local _codeword = "SHOEBILL"
 local _recipient = "FOCUS"
 local _nearestCity = "RAQQA"
-local _JTAC_Frequency = "97.1"
-local _JTAC_Callsign = "SHOEBILL"
+local _JTAC_Frequency = "69"
+local _JTAC_Callsign = "SPARTAN 3-1"
 local _heloType = "UH-60A"
 local _destination = "Tabqa"
 
@@ -38,10 +38,15 @@ Shoebill = {
         Start =
         _recipient .. ", [CALLSIGN]. Priority mission. A " .. _heloType .. " has been shot down near the city of " .. _nearestCity .. " by unknown hostiles. " ..
         "The helicopter was carrying a Special Ops group bound for " .. _destination .. ". They are requesting immediate CAS support and exfil. Coordinate with local " ..
-            "JTAC, callsign " ..
-            _JTAC_Callsign .. ", on frequency, " .. _JTAC_Frequency .. ", before dispatching a Cesar mission.",
+            "JTAC, callsign " .. _JTAC_Callsign .. ", on frequency, " .. _JTAC_Frequency .. ", before dispatching a Cesar mission.",
         CSAR =
-        _recipient .. ", [CALLSIGN]. Cesar dispatched.",
+            _recipient .. ", [CALLSIGN]. Cesar dispatched. E T A is twenty minutes. Ensure the area is secure.",
+        MissionComplete =
+            _recipient .. ", [CALLSIGN]. Shoebill succesfully extracted, mission accomplished.",
+        MissionFailed =
+            _recipient .. ", [CALLSIGN]. The entire Spec Ops team was killed, and the Cesar is R T B. Full debrief and A A R tomorrow at oh eight hundred.",
+        MissionFailedShotDown =
+        _recipient .. ", [CALLSIGN]. The Cesar helicopter was shot down as we failed to secure the L Z. Debriefing of wing leaders at oh eight hundred tomorrow morning.",
         }
 }
 
@@ -54,7 +59,7 @@ function Shoebill:Start(tts)
     self.TTS = tts
     self.Groups.BLU.Chinook:Activate()
     self.Groups.BLU.Chinook:Explode(150, 2)
-    self.Groups.BLU.Chinook:GetCoordinate():BigSmokeAndFire( 6, 1)
+    self.Groups.BLU.Chinook:GetCoordinate():BigSmokeAndFire( 5, 1)
     self.Groups.BLU.SpecOps:Activate()
     self.Groups.BLU.JTAC:Activate()
     DCAF.delay(function()
@@ -90,19 +95,36 @@ function Shoebill:SendActual(msg)
 end
 
 function Shoebill:Recover()
+    Debug("sausage -- Shoebill:Recover was activated")
+    self.Groups.BLU.JTAC:SetAIOn()
     DCAF.delay(function()
-        local jtac = self.Groups.BLU.JTAC:getGroup()
-        local spec = self.Groups.BLU.SpecOps:getGroup()
-        if jtac then
-            jtac:Destroy(true, 2)
-        end
-        if spec then
-            spec:Destroy(true, 2)
+        self.Groups.BLU.JTAC:Destroy()
+        self.Groups.BLU.SpecOps:Destroy()
+    end, 150)
+end
+
+function Shoebill:MissionComplete()
+    self:Send(self.MSG.MissionComplete)
+end
+
+function Shoebill:CheckLifeSpecOps()
+    self._checkLifeSchedulerSpec = DCAF.startScheduler(function()
+        local specs = self.Groups.BLU.SpecOps
+        if specs and not specs:IsActive() then return end
+        if not self.Groups.BLU.SpecOps:IsAlive() then
+            self:Send(self.MSG.MissionFailed)
         end
     end, 30)
 end
 
-function Shoebill:MissionComplete()
+function Shoebill:CheckLifeCSAR()
+    self._checkLifeSchedulerCsar = DCAF.startScheduler(function()
+        local csar = self.Groups.BLU.CSAR
+        if csar and not csar:IsActive() then return end
+        if not self.Groups.BLU.CSAR:IsAlive() then
+            self:Send(self.MSG.MissionFailedShotDown)
+        end
+    end, 30)
 end
 
 function Shoebill:CSAR()
@@ -113,22 +135,22 @@ function Shoebill:CSAR()
     end, 10)
 end
 
-function Shoebill:DropBomb()
-        local units = self.Groups.RED.InsInf:GetUnits()
-        local killUnits = math.floor(#units * 1)
-        for i = 1, killUnits, 1 do
-            local unit = self.Groups.RED.InsInf:GetUnit(i)
-            unit:Destroy()
-        end
-        self._boom_menu:Remove(true)
-end
+-- function Shoebill:DropBomb()
+--         local units = self.Groups.RED.InsInf:GetUnits()
+--         local killUnits = math.floor(#units * 1)
+--         for i = 1, killUnits, 1 do
+--             local unit = self.Groups.RED.InsInf:GetUnit(i)
+--             unit:Destroy()
+--         end
+--         self._boom_menu:Remove(true)
+-- end
 
 Shoebill._main_menu = GM_Menu:AddMenu(_codeword)
 Shoebill._start_menu = Shoebill._main_menu:AddCommand("Start", function()
     Shoebill:Start(TTS_Top_Dog)
-    Shoebill._boom_menu = Shoebill._main_menu:AddCommand("Boom", function()
-        Shoebill:DropBomb()
-    end)
+    -- Shoebill._boom_menu = Shoebill._main_menu:AddCommand("Boom", function()
+    --     Shoebill:DropBomb()
+    -- end)
     Shoebill._csar_menu = Shoebill._main_menu:AddCommand("CSAR", function()
         Shoebill:CSAR()
     end)
